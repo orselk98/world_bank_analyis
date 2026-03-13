b5fea3468a549429aae67270ae133e511a86ce6d
Orsel Kapllani <orsel.a.kapllani@gmail.com>
2026-03-14 00:01:22 +0100

    initial project setup and raw data load

---
+++ notebooks/analysis.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import os
+print(os.getcwd())
+
+
+
+def inspect_data(df):
+    print("Shape:",df.shape)
+
+    print("\nColumns Names:")
+    print(df.columns.tolist())
+
+    print("\nData Types:")
+    print(df.dtypes)
+
+    print(df.head())
+
+
+
+
+if __name__=="__main__":
+    df=pd.read_csv("data/raw/API_NY.GDP.MKTP.KD.ZG_DS2_en_csv_v2_107.csv", skiprows=4)
+    inspect_data(df)
