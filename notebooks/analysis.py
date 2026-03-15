bedbfaa2a90ddbe7da5633d27949d36253925c04
Orsel Kapllani <orsel.a.kapllani@gmail.com>
2026-03-15 22:45:53 +0100

    merge GDP data with country metadata, drop unnecesary columns

--- notebooks/analysis.py
+++ notebooks/analysis.py
@@ -26,24 +26,52 @@ def inspect_data(df):
 
     print("\nDataset Info:")
     print(df.info())
-    
-    print("\nNumber of NAN values in cleaned DataFrame:")
-    print(cleaned_df.isnull().sum())
 
+
+
+print("--"*50)
+
+def inspect_metadata(metadata_df):
+    print("Metadata Shape:",metadata_df.shape)
+    print("\nMetadata Column Names:")
+    print(metadata_df.columns.tolist())
+    print("\nMetadata Data Types:")
+    print(metadata_df.dtypes)
+    print("\nMetadata Sample:")
+    print(metadata_df.head())
+
+print("--"*50)
 
 def clean_data(df):
+    #Nan strategy: leave as it is - Missing GDP data is intentional
+    #(there are non reporting countries). pandas aggregations skip NaNs by default.
     df=df.drop("Unnamed: 70", axis=1)
     year_columns=[str(year) for year in range(1990,2024)]
     df=df[["Country Name","Country Code","Indicator Name","Indicator Code"]+year_columns]
     return df
 
-    #Nan strategy: leave as it is - Missing GDP data is intentional
-    #(there are non reporting countries). pandas aggregations skip NaNs by default.
+def merge_data(df, metadata_df):
+    merged_df=pd.merge(df, metadata_df, on="Country Code", how="left")
+
+    drop_columns=["SpecialNotes","TableName","Unnamed: 5"]
+    merged_df=merged_df.drop(columns=drop_columns, errors='ignore')
+    return merged_df
+
+   
 
 
 
 if __name__=="__main__":
     df=pd.read_csv("data/raw/API_NY.GDP.MKTP.KD.ZG_DS2_en_csv_v2_107.csv", skiprows=4)
+    metadata_df=pd.read_csv("data/raw/Metadata_Country_API_NY.GDP.MKTP.KD.ZG_DS2_en_csv_v2_107.csv")
     inspect_data(df)
+
     cleaned_df=clean_data(df)
+    inspect_data(cleaned_df)
+    
     print("\nCleaned DataFrame shape:",cleaned_df.shape)
+    inspect_metadata(metadata_df)
+    
+    merged_df=merge_data(cleaned_df, metadata_df)
+    print("\nMerged DataFrame shape:",merged_df.shape)
+    inspect_data(merged_df)
